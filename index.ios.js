e60e0c23cdb7020d6ce34160751e1a8c7037da23
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2016-07-22 11:11:22 -0300

    removido reactotron

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Root from './app/Root';
-import Reactotron from 'reactotron';
 
-Reactotron.connect({enabled: __DEV__})
 
 // Registrando o componente principal da aplicação
 AppRegistry.registerComponent('Hourz', ()=> Root);
